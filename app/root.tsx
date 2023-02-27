3dd472e21300dad4bf734a386b9c2789786349c7
Rohan <11038379+robase@users.noreply.github.com>
2023-02-27 13:28:39 +1100

    feat: install tailwind

--- app/root.tsx
+++ app/root.tsx
@@ -1,18 +1,15 @@
-import type { MetaFunction } from "@remix-run/node";
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+import type { LinksFunction, MetaFunction } from "@remix-run/node"
+import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react"
+
+import styles from "./tailwind.css"
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
   title: "New Remix App",
   viewport: "width=device-width,initial-scale=1",
-});
+})
+
+export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }]
 
 export default function App() {
   return (
@@ -28,5 +25,5 @@ export default function App() {
         <LiveReload />
       </body>
     </html>
-  );
+  )
 }
