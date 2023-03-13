a23b597d38c72e9153a6c04083988f01096e2de8
Rohan <11038379+robase@users.noreply.github.com>
2023-03-13 14:20:51 +1100

    chore: site name

--- app/root.tsx
+++ app/root.tsx
@@ -5,7 +5,7 @@ import styles from "./tailwind.css"
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "FHB Help",
+  title: "First Home Buyer Help: Get the real cost of your first home",
   description: "Calculate the costs of buying your first home",
   viewport: "width=device-width,initial-scale=1",
 })
