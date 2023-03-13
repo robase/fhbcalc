1ce0b79a287b2f1a771ca69a08942b91e7a26855
Rohan <11038379+robase@users.noreply.github.com>
2023-03-13 16:07:18 +1100

    chore: add analytics

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,6 @@
 import type { LinksFunction, MetaFunction } from "@remix-run/node"
 import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react"
+import { Analytics } from "@vercel/analytics/react"
 
 import styles from "./tailwind.css"
 
@@ -27,6 +28,7 @@ export default function App() {
         <Links />
       </head>
       <body>
+        <Analytics />
         <Outlet />
         <ScrollRestoration />
         <Scripts />
