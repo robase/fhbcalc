29443a0f97e03dc25c49eb347869c78ba43d3fed
Rohan <11038379+robase@users.noreply.github.com>
2023-03-15 14:19:28 +1100

    chore: remove scroll restoration - remove all cookies

--- app/root.tsx
+++ app/root.tsx
@@ -30,7 +30,7 @@ export default function App() {
       <body>
         <Analytics />
         <Outlet />
-        <ScrollRestoration />
+        {/* <ScrollRestoration /> */}
         <Scripts />
         <LiveReload />
       </body>
