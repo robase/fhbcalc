f7edf9b17b8be545b7de8b89652a2a9818bc3044
Rohan <11038379+robase@users.noreply.github.com>
2023-07-19 20:40:23 +1000

    chore: update remix

--- app/root.tsx
+++ app/root.tsx
@@ -1,21 +1,28 @@
-import type { LinksFunction, MetaFunction } from "@remix-run/node";
+import type { LinksFunction, V2_MetaFunction } from "@remix-run/node";
 import { Links, LiveReload, Meta, Outlet, Scripts } from "@remix-run/react";
 import { Analytics } from "@vercel/analytics/react";
-
+import cover from "./images/ogimage.jpg";
 import styles from "./tailwind.css";
 
-export const meta: MetaFunction = () => ({
-  charset: "utf-8",
-  title: "First Home Buyer Calculator",
-  description:
-    "Navigate the journey of buying your first home with ease. Discover your government scheme eligibility and the benefits to your home loan as a first home buyer",
-  viewport: "width=device-width,initial-scale=1",
-  "og:title": "First Home Buyer Calculator",
-  "og:description":
-    "Navigate the journey of buying your first home with ease. Discover your government scheme eligibility and the benefits to your home loan as a first home buyer",
-  "og:url": "https://firsthomebuyer.help",
-  "og:image": "https://firsthomebuyer.help/ogimage.jpg",
-});
+export const meta: V2_MetaFunction = () => [
+  { charset: "utf-8" },
+  { title: "First Home Buyer Calculator" },
+  {
+    description:
+      "Navigate the journey of buying your first home with ease. Discover your government scheme eligibility and the benefits to your home loan as a first home buyer",
+  },
+  { viewport: "width=device-width,initial-scale=1" },
+  { "og:title": "First Home Buyer Calculator" },
+  {
+    "og:description":
+      "Navigate the journey of buying your first home with ease. Discover your government scheme eligibility and the benefits to your home loan as a first home buyer",
+  },
+  { "og:url": "https://firsthomebuyer.help" },
+  {
+    "og:image":
+      process.env.NODE_ENV === "development" ? `http://localhost:3000${cover}` : `https://firsthomebuyer.help${cover}`,
+  },
+];
 
 export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
 
