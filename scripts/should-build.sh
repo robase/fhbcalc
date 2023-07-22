87a9a2771f28adf449e606ff0fde82212a93ad21
Rohan <11038379+robase@users.noreply.github.com>
2023-07-22 12:45:22 +1000

    feat: add codeowners and build protection

---
+++ scripts/should-build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
+
+if [[ "$VERCEL_GIT_COMMIT_REF" == "staging" || "$VERCEL_GIT_COMMIT_REF" == "main"  ]] ; then
+    echo "✅ - Build can proceed"
+    exit 1;
+else
+    echo "🛑 - Build only runs on dev or main"
+    exit 0;
+fi
